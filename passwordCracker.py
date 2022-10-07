0cb466e90b260e96ed7cd27079556747cbc670f1
Green <87390224+coder175@users.noreply.github.com>
2022-10-06 20:35:33 -0400

    Made Program Faster using Threads

--- passwordCracker.py
+++ passwordCracker.py
@@ -1,6 +1,4 @@
-import itertools
-import time
-import random
+import itertools, time, random, os
 from threading import Thread
 
 num = "0123456789"
@@ -13,7 +11,6 @@ password = ''
 for x in range(amount):
     password = password + str(random.randint(0, 9))
 print(password)
-global start, counter, charNum
 start = time.time()
 charNum = 1
 counter = 1
@@ -29,10 +26,12 @@ def updateProg():
         time.sleep(2.5 - ((time.time() - starttime) % 2.5))
 
 
-def passCrack():
-    for charNum in range(25):
+def passCrackOdd():
+    global charNum
+    for charNum in range(1,25,2):
         passwords = (itertools.product(num, repeat=charNum))
         for i in passwords:
+            global counter
             counter += 1
             i = str(i)
             i = i.replace("[", "")
@@ -49,9 +48,33 @@ def passCrack():
                 print('I used ' + "{:,}".format(counter) + ' tries.')
                 print('I used ' + "{:,}".format(end) + ' seconds to find the password.')
                 print('Also the password is ' + "{:,}".format(int(i)))
-                exit()
+                os._exit(0)
+def passCrackEven():
+    global charNum
+    for charNum in range(0, 25, 2):
+        passwords = (itertools.product(num, repeat=charNum))
+        for i in passwords:
+            global counter
+            counter += 1
+            i = str(i)
+            i = i.replace("[", "")
+            i = i.replace("]", "")
+            i = i.replace("'", "")
+            i = i.replace(" ", "")
+            i = i.replace(",", "")
+            i = i.replace("(", "")
+            i = i.replace(")", "")
+            if i == password:
+                end = round(time.time() - start)
+                timeTaken = end - start
+                print('\n \n')
+                print('I used ' + "{:,}".format(counter) + ' tries.')
+                print('I used ' + "{:,}".format(end) + ' seconds to find the password.')
+                print('Also the password is ' + "{:,}".format(int(i)))
+                os._exit(0)
 
 
 if __name__ == '__main__':
     Thread(target=updateProg).start()
-    Thread(target=passCrack).start()
+    Thread(target=passCrackOdd).start()
+    Thread(target=passCrackEven).start()
