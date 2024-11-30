a0a7c7d5e3acea567f815ac78bb553f49411e612
Adhitya Venkat <adhitya.venkat12@gmail.com>
2024-11-30 17:23:28 -0500

    Learning File

---
+++ percentage.py
@@ -0,0 +1,4 @@
+print('Give two numbers, and I will give the percent off!')
+a = int(input("Give the first price! "))
+b = int(input("Give the second price! "))
+print("Here is the percent off:", 100 - b/a*100, '%')
